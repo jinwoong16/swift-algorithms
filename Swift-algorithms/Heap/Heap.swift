bd4f61625728b2d4921b19c644509114448ad13f
jinwoong <baseball0020@gmail.com>
2023-02-06 19:00:15 +0900

    [data structure] heap

--- Swift-algorithms/Heap/Heap.swift
+++ Swift-algorithms/Heap/Heap.swift
@@ -6,3 +6,117 @@
 //
 
 import Foundation
+
+struct Heap<T> {
+  var nodes = [T]()
+  private var orderCriteria: (T, T) -> Bool
+  
+  init(sort: @escaping (T, T) -> Bool) {
+    self.orderCriteria = sort
+  }
+  
+  init(array: [T], sort: @escaping (T, T) -> Bool) {
+    self.orderCriteria = sort
+    configureHeap(from: array)
+  }
+  
+  var isEmpty: Bool {
+    return nodes.isEmpty
+  }
+  
+  var count: Int {
+    return nodes.count
+  }
+  
+  func parentIndex(ofIndex i: Int) -> Int {
+    return (i - 1) / 2
+  }
+  
+  func leftChildIndex(ofIndex i: Int) -> Int {
+    return 2*i + 1
+  }
+  
+  func rightChildIndex(ofIndex i: Int) -> Int {
+    return 2*i + 2
+  }
+  
+  private mutating func configureHeap(from array: [T]) {
+    nodes = array
+    for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+      shiftDown(i)
+    }
+  }
+
+  func peek() -> T? {
+    return nodes.first
+  }
+  
+  mutating func shiftUp(_ index: Int) {
+    var childIndex = index
+    let child = nodes[childIndex]
+    var parentIndex = self.parentIndex(ofIndex: childIndex)
+    
+    while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+      nodes[childIndex] = nodes[parentIndex]
+      childIndex = parentIndex
+      parentIndex = self.parentIndex(ofIndex: childIndex)
+    }
+    
+    nodes[childIndex] = child
+  }
+  
+  mutating func shiftDown(from index: Int, until endIndex: Int) {
+    let leftChildIndex = self.leftChildIndex(ofIndex: index)
+    let rightChildIndex = leftChildIndex + 1
+    
+    var first = index
+    
+    if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+      first = leftChildIndex
+    }
+    if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+      first = rightChildIndex
+    }
+    
+    if first == index { return }
+    
+    nodes.swapAt(index, first)
+    shiftDown(from: first, until: endIndex)
+  }
+  
+  mutating func shiftDown(_ index: Int) {
+    shiftDown(from: index, until: nodes.count)
+  }
+  
+  mutating func insert(_ value: T) {
+    nodes.append(value)
+    shiftUp(nodes.count - 1)
+  }
+  
+  mutating func remove() -> T? {
+    guard !nodes.isEmpty else { return nil }
+    
+    if nodes.count == 1 {
+      return nodes.removeFirst()
+    } else {
+      let value = nodes[0]
+      nodes[0] = nodes.removeLast()
+      shiftDown(0)
+      
+      return value
+    }
+  }
+  
+  mutating func remove(at index: Int) -> T? {
+    guard index < nodes.count else { return nil }
+    
+    let size = nodes.count - 1
+    if index != size {
+      nodes.swapAt(index, size)
+      shiftDown(from: index, until: size)
+      shiftUp(index)
+    }
+    
+    return nodes.removeLast()
+  }
+}
