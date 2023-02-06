b0255960e054a33d8b9ce4dfbe023d217a66a2bf
jinwoong <baseball0020@gmail.com>
2023-02-06 19:02:12 +0900

    [graph] deapthFirstSearch with graph

--- Swift-algorithms/Graph/DFS/deapthFristSearch.swift
+++ Swift-algorithms/Graph/DFS/deapthFristSearch.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+
+func deapthFirstSearch<T>(_ graph: AbstractGraph<T>, source: Vertex<T>) -> [T] {
+  var stack = Stack<Vertex<T>>()
+  var visited = [Vertex<T>: Bool]()
+  stack.push(source)
+  
+  var nodesExplored = [source.data]
+  visited[source] = true
+  
+  while !stack.isEmpty {
+    if let node = stack.pop() {
+      if !visited[node, default: false] {
+        visited[node] = true
+        nodesExplored.append(node.data)
+      }
+      
+      for edge in graph.edgesFrom(node).reversed() {
+        let neighborNode = edge.to
+        if !visited[neighborNode, default: false] {
+          stack.push(neighborNode)
+        }
+      }
+    }
+  }
+  
+  return nodesExplored
+}
+
+func testDFSWithListGraph() {
+  let graph = AdjacencyListGraph<String>()
+
+  let a = graph.createVertex("a")
+  let b = graph.createVertex("b")
+  let c = graph.createVertex("c")
+  let d = graph.createVertex("d")
+  let e = graph.createVertex("e")
+  let f = graph.createVertex("f")
+  let g = graph.createVertex("g")
+  let h = graph.createVertex("h")
+
+  graph.addUndirectedEdge((a, b), withWeight: nil)
+  graph.addUndirectedEdge((a, c), withWeight: nil)
+  graph.addUndirectedEdge((b, d), withWeight: nil)
+  graph.addUndirectedEdge((b, e), withWeight: nil)
+  graph.addUndirectedEdge((c, f), withWeight: nil)
+  graph.addUndirectedEdge((c, g), withWeight: nil)
+  graph.addUndirectedEdge((e, h), withWeight: nil)
+  graph.addUndirectedEdge((e, f), withWeight: nil)
+  graph.addUndirectedEdge((f, g), withWeight: nil)
+
+  let nodesExplored = deapthFirstSearch(graph, source: a)
+  print(nodesExplored)
+}
+
+func testDFSWithMatrixGraph() {
+  let matrix = AdjacencyMatrixGraph<String>()
+
+  let ma = matrix.createVertex("a")
+  let mb = matrix.createVertex("b")
+  let mc = matrix.createVertex("c")
+  let md = matrix.createVertex("d")
+  let me = matrix.createVertex("e")
+  let mf = matrix.createVertex("f")
+  let mg = matrix.createVertex("g")
+  let mh = matrix.createVertex("h")
+
+  matrix.addUndirectedEdge((ma, mb), withWeight: 1)
+  matrix.addUndirectedEdge((ma, mc), withWeight: 1)
+  matrix.addUndirectedEdge((mb, md), withWeight: 1)
+  matrix.addUndirectedEdge((mb, me), withWeight: 1)
+  matrix.addUndirectedEdge((mc, mf), withWeight: 1)
+  matrix.addUndirectedEdge((mc, mg), withWeight: 1)
+  matrix.addUndirectedEdge((me, mh), withWeight: 1)
+  matrix.addUndirectedEdge((me, mf), withWeight: 1)
+  matrix.addUndirectedEdge((mf, mg), withWeight: 1)
+
+  let matrixNodesExplored = deapthFirstSearch(matrix, source: ma)
+  print(matrixNodesExplored)
+  
+  print(matrix.edgesFrom(me))
+}
